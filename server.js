f5cd86defe2fbe58c0b01db7ef6cc7f6fa6a7167
Van <vango85@gmail.com>
2020-05-04 19:14:55 +0200

    Post codealong changes

--- server.js
+++ server.js
@@ -1,29 +1,48 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-
-const port = process.env.PORT || 8080;
+import express from 'express';
 const app = express();
 
-const data = [
-  { name: "Bob", age: 34 },
-  { name: "Ivan", age: 22 },
-  { name: "Karl", age: 42 },
-  { name: "Anton", age: 21 },
+const port = process.env.PORT || 8080;
+
+const people = [
+  { id: 3, name: 'Bob', age: 34 },
+  { id: 2, name: 'Ivan', age: 22 },
+  { id: 4, name: 'Van', age: 34 },
+  { id: 1, name: 'Karl', age: 42 },
+  { id: 0, name: 'Anton', age: 21 },
 ];
 
-app.use(cors());
-app.use(bodyParser.json());
-
-app.get("/people", (req, res) => {
-  res.send(data);
+app.get('/', (request /*Incoming*/, response /*Outgoing*/) => {
+  // Any code we want
+  // - Database connections
+  // - Complicated operations
+  // - Data lookup
+  // - Third party API requests
+  response.send('Hello world');
 });
 
-app.get("/people/:index", (req, res) => {
-  const { index } = req.params;
-  res.send(data[index]);
+// With "Query Parameter"
+// localhost:8080/people?name=Ivan
+app.get('/people', (req, res) => {
+  const { name } = req.query;
+
+  const personsFound = people.filter((person) => person.name.includes(name));
+
+  res.send(personsFound);
+});
+
+// With "Path Parameter"
+app.get('/people/:id', (req, res) => {
+  const { id } = req.params; // ID is a string here
+  // const index = req.params.index;
+  console.log(`The req.params are: ${JSON.stringify(req.params)}`);
+
+  const personFound = people.find(
+    (person) => person.id === +id /* Convert to num */
+  );
+
+  res.send(personFound);
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log('Hello World, the server is running');
 });
