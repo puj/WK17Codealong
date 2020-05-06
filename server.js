bb7f6910247c071b66a75d93c291a4f643ea0f71
Van <vango85@gmail.com>
2020-05-06 11:30:25 +0200

    [xx] After live session

--- server.js
+++ server.js
@@ -1,47 +1,53 @@
-import express from "express";
-import sales from "./data/avocado-sales.json";
-import { start } from "repl";
+import express from 'express';
+import sales from './data/avocado-sales.json';
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-// With "Query Parameter"
-app.get("/sales", (req, res) => {
-  // How to implement paging here?
+// /sales endpoint
+// PARAMETERS:
+//  - page (minValue:0, default: 0): a number indicating which page of the results to return
+//     usage: localhost:8080/?page=2
+//  - pageSize (default: 20) : a number indicating how many results per page
+// RETURNS:  all sales from avocado-sales.json
+app.get('/sales', (req, res) => {
+  // const { page } = req.query; // req.query === {page:1}
+  // ?? allows us to specify a default value
+  const page = req.query.page ?? 0;
 
-  // Deconstruct which page to show
-  const { page } = req.query;
-
-  // Attempt to retrieve pageSize from query
+  // 20 is default value, maybe should be it's own constant
   const pageSize = req.query.pageSize ?? 20;
 
-  // Calculate start index
+  // Calculate the start index
   const startIndex = page * pageSize;
 
-  // Calculate and bound end index
-  const endIndex = Math.min(startIndex + pageSize, sales.length);
+  // Calculate the end index
+  const endIndex = startIndex + +pageSize;
 
   // Slice results
-  const results = sales.slice(startIndex, endIndex);
+  const salesForPage = sales.slice(startIndex, endIndex);
 
-  // Inform the client
-  res.send({
+  // Return 404 if no results
+  if (salesForPage.length === 0) {
+    // Send 404 error to client
+    res.status(404).send({ error: 'No results found' });
+
+    // Do not continue executing code in this endpoint
+    return;
+  }
+
+  // Create return object with information and results
+  const returnObject = {
     pageSize: pageSize,
     page: page,
     maxPages: parseInt(sales.length / pageSize),
-    results: results,
-  });
-});
+    results: salesForPage,
+  };
 
-// With "Path Parameter"
-app.get("/sales/:id", (req, res) => {
-  const { id } = req.params; // ID is a string here
-  const saleFound = sales.find((sale) => sale.id === +id /* Convert to num */);
-
-  // What happens if not found?
-  res.send(saleFound);
+  // Send return object back to the client
+  res.send(returnObject);
 });
 
 app.listen(port, () => {
-  console.log("Hello World, the server is running");
+  console.log('Hello World, the server is running');
 });
